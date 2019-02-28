c3db94b23653c40628c7849ba859eaf105d4622b
Christoffer Petersen <ctrpetersen@outlook.com>
2019-02-28 11:05:34 +0100

    random prefix now based on avg

--- Gen/NameGen.cs
+++ Gen/NameGen.cs
@@ -8,9 +8,31 @@ namespace DOG.Gen
     {
         private List<string> _dogPrefixes = new List<string>
         {
-            "Sir",
-            "Lord",
-            "Count"
+            "Emperor",
+            "Caesar",
+            "Tsar",
+            "King of Kings",
+            "High King",
+            "Queen",
+            "Prince",
+            "Caliph",
+            "Archduke",
+            "Grand Duke",
+            "Grand Prince",
+            "Duke",
+            "Prince",
+            "Marquess",
+            "Landgrave",
+            "Count",
+            "Viscount",
+            "Baron",
+            "Baronet",
+            "Dominus",
+            "Knight",
+            "Laird",
+            "Esquire",
+            "Gentleman",
+            "Duchess"
         };
 
         private List<string> _dogNames = new List<string>
@@ -20,7 +42,215 @@ namespace DOG.Gen
             "Bones",
             "Fido",
             "Bella",
-            "Beauty"
+            "Beauty",
+            "Tail",
+            "Paws",
+            "Star",
+            "Woofer",
+            "Borker",
+            "Pupper",
+            "Puppo",
+            "Longboye",
+            "Ace",
+            "Apollo",
+            "Bailey",
+            "Bandit",
+            "Baxter",
+            "Bear",
+            "Beau",
+            "Benji",
+            "Benny",
+            "Bentley",
+            "Blue",
+            "Bo",
+            "Boomer",
+            "Brady",
+            "Brody",
+            "Bruno",
+            "Brutus",
+            "Bubba",
+            "Buddy",
+            "Buster",
+            "Cash",
+            "Champ",
+            "Chance",
+            "Charlie",
+            "Chase",
+            "Chester",
+            "Chico",
+            "Coco",
+            "Cody",
+            "Cooper",
+            "Copper",
+            "Dexter",
+            "Diesel",
+            "Duke",
+            "Elvis",
+            "Finn",
+            "Frankie",
+            "George",
+            "Gizmo",
+            "Gunner",
+            "Gus",
+            "Hank",
+            "Harley",
+            "Henry",
+            "Hunter",
+            "Jack",
+            "Jackson",
+            "Jake",
+            "Jasper",
+            "Jax",
+            "Joey",
+            "Kobe",
+            "Leo",
+            "Loki",
+            "Louie",
+            "Lucky",
+            "Luke",
+            "Mac",
+            "Marley",
+            "Max",
+            "Mickey",
+            "Milo",
+            "Moose",
+            "Murphy",
+            "Oliver",
+            "Ollie",
+            "Oreo",
+            "Oscar",
+            "Otis",
+            "Peanut",
+            "Prince",
+            "Rex",
+            "Riley",
+            "Rocco",
+            "Rocky",
+            "Romeo",
+            "Roscoe",
+            "Rudy",
+            "Rufus",
+            "Rusty",
+            "Sam",
+            "Sammy",
+            "Samson",
+            "Scooter",
+            "Scout",
+            "Shadow",
+            "Simba",
+            "Sparky",
+            "Spike",
+            "Tank",
+            "Teddy",
+            "Thor",
+            "Toby",
+            "Tucker",
+            "Tyson",
+            "Vader",
+            "Winston",
+            "Yoda",
+            "Zeus",
+            "Ziggy",
+            "Abby",
+            "Allie",
+            "Angel",
+            "Annie",
+            "Athena",
+            "Baby",
+            "Bailey",
+            "Bella",
+            "Belle",
+            "Bonnie",
+            "Brandy",
+            "Cali",
+            "Callie",
+            "Casey",
+            "Charlie",
+            "Chloe",
+            "Cleo",
+            "Coco",
+            "Cocoa",
+            "Cookie",
+            "Daisy",
+            "Dakota",
+            "Dixie",
+            "Ella",
+            "Ellie",
+            "Emma",
+            "Gigi",
+            "Ginger",
+            "Grace",
+            "Gracie",
+            "Hannah",
+            "Harley",
+            "Hazel",
+            "Heidi",
+            "Holly",
+            "Honey",
+            "Izzy",
+            "Jasmine",
+            "Josie",
+            "Katie",
+            "Kona",
+            "Lacey",
+            "Lady",
+            "Layla",
+            "Lexi",
+            "Lexie",
+            "Lilly",
+            "Lily",
+            "Lola",
+            "Lucky",
+            "Lucy",
+            "Lulu",
+            "Luna",
+            "Macy",
+            "Maddie",
+            "Madison",
+            "Maggie",
+            "Marley",
+            "Maya",
+            "Mia",
+            "Millie",
+            "Mimi",
+            "Minnie",
+            "Missy",
+            "Misty",
+            "Mocha",
+            "Molly",
+            "Nala",
+            "Nikki",
+            "Olive",
+            "Peanut",
+            "Pebbles",
+            "Penny",
+            "Pepper",
+            "Phoebe",
+            "Piper",
+            "Princess",
+            "Riley",
+            "Rosie",
+            "Roxie",
+            "Roxy",
+            "Ruby",
+            "Sadie",
+            "Sally",
+            "Sandy",
+            "Sasha",
+            "Sassy",
+            "Scout",
+            "Shadow",
+            "Shelby",
+            "Sierra",
+            "Sophie",
+            "Stella",
+            "Sugar",
+            "Sydney",
+            "Trixie",
+            "Willow",
+            "Winnie",
+            "Zoe",
+            "Zoey"
         };
 
         private List<string> _dogSuffixDefense = new List<string>
@@ -43,7 +273,8 @@ namespace DOG.Gen
         {
             "Smart",
             "Big-brained",
-            "Wise"
+            "Wise",
+            "Mage"
         };
 
         private List<string> _dogSuffixWill = new List<string>
@@ -78,39 +309,49 @@ namespace DOG.Gen
             "Destroyer of Worlds",
             "Eater of Souls",
             "Ancient One",
-            "Forsaken"
+            "Forsaken",
+            "Exiled One"
         };
+
+        private const int GodlikeMin = 85;
+        private const int UselessMax = 15;
 
         internal string GenerateDogName(Dog dog)
         {
             var rnd = new Random(Guid.NewGuid().GetHashCode());
             var name = "";
-            var godlikeMin = 85;
-            var uselessMax = 15;
 
-            if (rnd.Next(0, 100) < 20)
+            var prefixChance = (float)(dog.Prayer + dog.AtkPower + dog.Defense + dog.Health + dog.Intelligence + dog.Will) / 6 * 0.01F;
+            prefixChance = prefixChance * (prefixChance - 0.7F);
+            
+
+            foreach (var t in _dogPrefixes)
             {
-                name += _dogPrefixes[rnd.Next(0, _dogPrefixes.Count)] + " ";
+                if (rnd.NextDouble() < prefixChance)
+                {
+                    name += t + " ";
+                    break;
+                }
             }
 
             name += _dogNames[rnd.Next(0, _dogNames.Count)];
 
-            if (dog.AtkPower < uselessMax && 
-                dog.Defense < uselessMax && 
-                dog.Health < uselessMax && 
-                dog.Intelligence < uselessMax && 
-                dog.Will < uselessMax &&
-                dog.Prayer < uselessMax)
+            if (dog.AtkPower < UselessMax && 
+                dog.Defense < UselessMax && 
+                dog.Health < UselessMax && 
+                dog.Intelligence < UselessMax && 
+                dog.Will < UselessMax &&
+                dog.Prayer < UselessMax)
             {
                 name += " the " + _dogSuffixUseless[rnd.Next(0, _dogSuffixUseless.Count)];
             }
 
-            else if (dog.AtkPower > godlikeMin && 
-                     dog.Defense > godlikeMin && 
-                     dog.Health > godlikeMin && 
-                     dog.Intelligence > godlikeMin &&
-                     dog.Will > godlikeMin && 
-                     dog.Prayer > godlikeMin)
+            else if (dog.AtkPower > GodlikeMin && 
+                     dog.Defense > GodlikeMin && 
+                     dog.Health > GodlikeMin && 
+                     dog.Intelligence > GodlikeMin &&
+                     dog.Will > GodlikeMin && 
+                     dog.Prayer > GodlikeMin)
             {
                 name += " the " + _dogSuffixGodlike[rnd.Next(0, _dogSuffixGodlike.Count)];
             }
